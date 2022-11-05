cb7eb16ea3829009fa80b6c49402e437f09851f8
jeanpi156 <jalbinobetancourt1@babson.edu>
2022-11-04 20:27:39 -0400

    assignment2_v2.py

--- movies.py
+++ movies.py
@@ -4,6 +4,7 @@ Make a program that examines imdb reviews of a movie that was considered to "flo
         a) The purpose of this is to gauge why the movie was not successfull.
     2) Use natural language to gauge the sentiment of each review.
 Each entry will be stored in a dictionary depending on how high the sentiment score for that emotion was.
+
 """
 from imdb import Cinemagoer
 import nltk
@@ -12,65 +13,80 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
+
 def movie_id(movie):
     """
-    Returns IMDB movie code.  Movie inpit must be string.
+    Returns IMDB movie code.  Movie inpit must be string. 
     """
     ia = Cinemagoer()
-    # if type(movie) != str:
-    #     raise Exception('Sorry, Movie must be a string.  Try using ""')
-    # else:
     name = ia.search_movie(movie)[0]
     id = name.movieID
     return id
-# print(movie_id('The Matrix Resurrections'))
+
 matrixid = 10838180
 def sent_movie_reviews():
+    """
+    Split movie reviews into sentences
+    """
     ia = Cinemagoer()
     data = ia.get_movie_reviews(10838180)
     rev_list = data['data']['reviews'] #rev_list is a list of dictionaries
-    # print(len(rev_list))
     sent_rev = []
     review = []
     for i in range(len(rev_list)):
         dictionary = rev_list[i-1]
         review.append(dictionary['content'])
-        # for review in dictionary:
-        # print(review)
         for sentence in review:
             sent_rev.append(sent_tokenize(sentence)) 
-            # print(sentence)
     return sent_rev # List of lists of sentences
-# print(sent_movie_reviews())
+
 def word_movie_reviews():
+    """
+    Split movie reviews into lists of words in a sentence
+    """
     data = sent_movie_reviews()
     result = []
     for lst in data:
-        for sentence in lst:
-            result.append(word_tokenize(sentence))
+        for word in lst:
+            result.append(word_tokenize(word))
     return result
+
 def remove_fluff():
+    """
+    Search each word in the list of lists on word_movie_reviews() for each word and include it in filtered
+    reviews if the word is not a stopword
+    """
     filtered_rev = []
     fluff = tuple(stopwords.words("english"))
     review = word_movie_reviews()
-    for word in review:
-        if word not in fluff:
-            filtered_rev.append(word)
-    return filtered_rev
-# print(remove_fluff())
+    for lst in range(len(review)):
+        line = review[lst]
+        for i in range(len(line)):
+            word = line[i]
+            if word not in fluff:
+                filtered_rev.append(word)
+    return filtered_rev 
+def word_count():
+    res = 0
+    review = word_movie_reviews()
+    for lst in range(len(review)):
+        line = review[lst]
+        for i in range(len(line)):
+            word = line[i]
+            res += (len(word) - (len(word)-1))
+    return res
+
 def make_hist():
     hist = {}
     data = remove_fluff()
-    # print(type(data))
-    for review in data:
-        for word in review:
-            word = word.lower()
-            if word not in hist:
-                hist[word] = 1
-            else:
-                hist[word] += 1 
+    for word in data:
+        word = word.lower()
+        if word not in hist:
+            hist[word] = 1
+        else:
+            hist[word] += 1 
     return hist
-# print(make_hist())
+
 def order_hist():
     hist = make_hist()
     inverse = {}
@@ -79,7 +95,7 @@ def order_hist():
     most_common = sorted(list(inverse.items()), reverse = True)
     return most_common
 
-def sentiment(): # I made this function into regular code in order to speed things up (running this takes a lot of time)
+def sentiment(): 
     """
     Get sentiment score of each sentence in a review
     """
@@ -95,6 +111,12 @@ def sentiment(): # I made this function into regular code in order to speed thin
 def average(lst):
     return sum(lst)/len(lst)
 def average_score(scores):
+    """
+    This fucntion takes every sentence's score (dictionary) and 
+    makes a list out of it (hence making a summary of sent scores for a review)
+    It then calculates an average of the values of each key in each dict in the list
+    This average is the score of the review.
+    """
     pos_score = []
     neg_score = []
     neu_score = []
@@ -111,19 +133,13 @@ def average_score(scores):
     perc_com = sum(com_score)/total_scores
     absolute_avg = {'pos': perc_pos, 'neg': perc_neg, 'neu': perc_neu, 'compound': perc_com}
     categorical_avg = {'pos': average(pos_score), 'neg': average(neg_score), 'neu': average(neu_score), 'compound': average(com_score)}
+    # catergorical_avg was used to compare the actual statistic with the simple average of each category
     return absolute_avg
 
-"""
-I want to take every sentence's score (dictionary) and 
-make a list out of it (hence making a summary of sent scores for a review)
-I would then calc an average of the values of each key in each dict in the list
-This average would be the score of the review
-"""
+
 def main():
-    # print(average_score(sentiment()))
-    # print(order_hist())
-    print(len(word_movie_reviews()))
-    print(len(stopwords.words("english")))
-    print(len(remove_fluff()))
+    print(average_score(sentiment()))
+    print(order_hist())
+
 if __name__ == "__main__":
     main()
